b89a5dab6c1323341abf6f0f4b6ffa4e436be03f
Manasverma1111 <vmanas79@gmail.com>
2026-03-09 23:52:45 +0530

    [manas Verma] add . added UC10- Generic Quantity

---
+++ src/main/java/org/example/IMeasurable.java
@@ -0,0 +1,13 @@
+package src.main.java.org.example;
+
+
+public interface IMeasurable {
+
+    double getConversionFactor();
+
+    double convertToBaseUnit(double value);
+
+    double convertFromBaseUnit(double baseValue);
+
+    String getUnitName();
+}
