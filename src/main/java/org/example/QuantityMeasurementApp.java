d869759447a7a68233509c76314860473068ae72
Manasverma1111 <vmanas79@gmail.com>
2026-03-09 23:35:46 +0530

    [manas Verma] add . added UC7- Addition With target Unit

--- src/main/java/org/example/QuantityMeasurementApp.java
+++ src/main/java/org/example/QuantityMeasurementApp.java
@@ -4,22 +4,22 @@ public class QuantityMeasurementApp {
 
     public static void main(String[] args) {
 
-        QuantityLength q1 = new QuantityLength(1.0, LengthUnit.FEET);
-        QuantityLength q2 = new QuantityLength(12.0, LengthUnit.INCHES);
+        QuantityLength a = new QuantityLength(1.0, LengthUnit.FEET);
+        QuantityLength b = new QuantityLength(12.0, LengthUnit.INCHES);
 
-        System.out.println("Input: add(" + q1 + ", " + q2 + ")");
-        System.out.println("Output: " + q1.add(q2));
+        QuantityLength result =
+                QuantityLength.add(a, b, LengthUnit.YARDS);
 
-        QuantityLength q3 = new QuantityLength(1.0, LengthUnit.YARDS);
-        QuantityLength q4 = new QuantityLength(3.0, LengthUnit.FEET);
+        System.out.println("Input: add(" + a + ", " + b + ", YARDS)");
+        System.out.println("Output: " + result);
 
-        System.out.println("Input: add(" + q3 + ", " + q4 + ")");
-        System.out.println("Output: " + q3.add(q4));
+        QuantityLength x = new QuantityLength(36.0, LengthUnit.INCHES);
+        QuantityLength y = new QuantityLength(1.0, LengthUnit.YARDS);
 
-        QuantityLength q5 = new QuantityLength(2.54, LengthUnit.CENTIMETERS);
-        QuantityLength q6 = new QuantityLength(1.0, LengthUnit.INCHES);
+        QuantityLength result2 =
+                QuantityLength.add(x, y, LengthUnit.FEET);
 
-        System.out.println("Input: add(" + q5 + ", " + q6 + ")");
-        System.out.println("Output: " + q5.add(q6));
+        System.out.println("Input: add(" + x + ", " + y + ", FEET)");
+        System.out.println("Output: " + result2);
     }
 }
