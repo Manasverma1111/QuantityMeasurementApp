66a71dac8cba9a91e0e486520ade2c7029d8dfa0
Manasverma1111 <vmanas79@gmail.com>
2026-03-13 16:02:27 +0530

    [manas Verma] add . added UC16- Database Integration

---
+++ quantitymeasurementapp/src/main/java/org/example/repository/IQuantityMeasurementRepository.java
@@ -0,0 +1,13 @@
+package src.main.java.org.example.repository;
+
+
+import src.main.java.org.example.entity.QuantityMeasurementEntity;
+
+import java.util.List;
+
+public interface IQuantityMeasurementRepository {
+
+    void save(QuantityMeasurementEntity entity);
+
+    List<QuantityMeasurementEntity> getAllMeasurements();
+}
